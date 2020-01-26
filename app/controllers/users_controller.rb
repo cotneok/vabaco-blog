91300e2081006f01998ac68b58f15f393fbd886c
Tsotne <cotne.ok@gmail.com>
2020-01-26 21:24:09 +0400

    Complete user admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -49,6 +50,12 @@ class UsersController < ApplicationController
   def require_same_user
     if current_user != @article.user
       flash[:danger] = "You can only edit our own account"
+      redirect_to root_path
+    end
+  end
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admin users can perform that action"
       redirect_to root_path
     end
   end
