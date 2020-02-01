4bdc1f3b26be38427a1ba98cea488ff99881c9ad
Tsotne <cotne.ok@gmail.com>
2020-02-01 16:55:21 +0400

    Add article categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_210723) do
+ActiveRecord::Schema.define(version: 2020_02_01_125032) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
