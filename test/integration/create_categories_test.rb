024ed75eec8f1a354dc100db412c01ff71c9e6e3
Tsotne <cotne.ok@gmail.com>
2020-02-01 16:40:46 +0400

    Add tests and ui inerface for categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class CreateCaegoriesTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  end
+
   test "get new category form and create category" do
+  sign_in_as(@user, "password")
   get new_category_path
   assert_template "categories/new"
   assert_difference "Category.count", 1 do
@@ -13,14 +18,14 @@ class CreateCaegoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "invalid category submission results in failure" do
+    sign_in_as(@user, "password")
     get new_category_path
-    assert_template 'categories/new'
-    assert_no_difference 'Category.count' do
-      post categories_path, params: { category: {name: " "} }
-    end
+    assert_template "categories/new"
+  assert_no_difference 'Category.count' do
+    post categories_path, params: { category: {name: " "} }
+  end
     assert_template 'categories/new'
     assert_select 'h2.panel-title'
     assert_select 'div.panel-body'
   end
-
 end
