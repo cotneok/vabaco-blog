c1577a037c2822578c9d3c790e3562399d1c35fd
Tsotne <cotne.ok@gmail.com>
2020-02-03 08:13:14 +0400

    Add last changes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,9 +20,24 @@ class CategoriesController < ApplicationController
   end
 
   def show
-
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+    flash[:success] = "Category name was successfully updated"
+    redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+  
   private
   def category_params
     params.require(:category).permit(:name)
@@ -32,5 +47,5 @@ class CategoriesController < ApplicationController
       flash[:danger] = "Only admins can perform that action"
       redirect_to categories_path
     end
-  end 
+  end
 end
